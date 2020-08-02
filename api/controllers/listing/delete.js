44f5cd3593946dcd0c5cd19e48b290bb8cbe3a50
Achilles45 <achillesusuoyibo7@gmail.com>
2020-08-02 21:17:17 +0100

    Compelted CRUD functionality for hostel listers

---
+++ api/controllers/listing/delete.js
@@ -0,0 +1,53 @@
+module.exports = {
+
+
+  friendlyName: 'Delete',
+
+
+  description: 'Delete a listing.',
+
+
+  inputs: {
+    id:{
+      type: "string"
+    }
+  },
+
+
+  exits: {
+    success:{
+      description: "Successfully deleted"
+    },
+    invalidRequest:{
+      description: "Invalid request"
+    },
+    unauthorized:{
+      description: "Not authorized"
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+    try {
+      //First get the ID of the current login user
+      const userId = this.req.me.id;
+
+      //Find the property to be deleted
+      let listing = await Listing.findOne({id: inputs.id});
+      
+      //Now check if this listing belongs to this user
+      if(listing.lister != userId){
+        return exits.unauthorized({message: "You can\'t be serious. This is not your listing man. Joke intended"})
+      }
+
+      //Else delete the listing
+      const listingToBeDeleted = await Listing.destroy({id: inputs.id});
+      return exits.success({
+        message: "Listing successfully deleted"
+      })
+      
+    } catch (error) {
+      
+    }
+  }
+};
---
+++ api/controllers/listing/update.js
@@ -0,0 +1,71 @@
+module.exports = {
+
+
+  friendlyName: 'Update',
+
+
+  description: 'Update a listing.',
+
+
+  inputs: {
+    id:{
+      type: "string",
+      required: true
+    },
+    name: {
+      type: "string",
+      required: true
+    },
+    type:{
+      type: "string",
+      required: true
+    },
+    address:{
+      type: "string",
+      required: true
+    },
+    rent:{
+      type: "string",
+      required: true
+    }
+  },
+
+
+  exits: {
+    success:{
+      description: "Successful"
+    },
+    notAuthorized:{
+      description: "Not authorized"
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+    //First get the ID of the logged in user
+    let userId = this.req.me.id;
+
+    //Find the particular listing to be updated
+    let listing = await Listing.findOne({id: inputs.id})
+
+    //Now check if the listing found above has same lister value as the userId
+    if(listing.lister != userId)
+    return exits.notAuthorized({
+      message: "You are not authorized to make this request",
+    })
+
+    //So edit the listing since it belongs to this currecnt logged in user
+    const updatedListing = await Listing.update({id: inputs.id}).set({
+      name: inputs.name,
+      address: inputs.address,
+      type: inputs.type,
+      rent: inputs.rent
+    }).fetch();
+    return exits.success({
+      message: "Listing was successfully updated",
+      data:updatedListing
+    })
+    
+  }
+
+};
